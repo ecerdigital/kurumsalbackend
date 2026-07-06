da79ec7b1cc0b5c677819f9e0547f975e60e420f
ecerdigital <kerem@ecer.com.tr>
2026-07-06 16:18:22 +0300

    feat: URLs - RoleViewSet eklendi

--- personel/urls.py
+++ personel/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EmployeeViewSet
+from .views import EmployeeViewSet, RoleViewSet
 
 router = DefaultRouter()
 router.register(r'', EmployeeViewSet, basename='personel')
+router.register(r'roles', RoleViewSet, basename='roles')
 
 urlpatterns = [
     path('', include(router.urls)),
